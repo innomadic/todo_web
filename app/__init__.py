ad0d965a2b77d320691b3fc3582d6adcf3229ef3
= <nick@innomadic.com>
2021-07-03 17:57:08 +0300

    Removed the duplicate import

--- app/__init__.py
+++ app/__init__.py
@@ -1,42 +1,42 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import os 
+import os
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_bootstrap import Bootstrap
-import os
 import re
 
 app = Flask(__name__)
 Bootstrap(app)
 
-# config for forms 
+# config for forms
 
 # this key should not be in souce code for production
 # create a .env file and set your own
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'secret321'
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY") or "secret321"
 
 # config for db
 basedir = os.path.abspath(os.path.dirname(__file__))
- 
 
 
 uri = os.getenv("DATABASE_URL")  # or other relevant config var
 if uri is not None:
     if uri.startswith("postgres://"):
         uri = uri.replace("postgres://", "postgresql://", 1)
-    app.config['SQLALCHEMY_DATABASE_URI'] = uri
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri
 else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
+        basedir, "app.db"
+    )
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
 
 
-#config for login manager
+# config for login manager
 
 login = LoginManager(app)
 login.login_view = "login"
